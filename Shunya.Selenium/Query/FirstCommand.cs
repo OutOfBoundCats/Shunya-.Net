ad52831951f54696d0a5cb4feeed6c1d36c8c444
raj <floatraj@gmail.com>
2023-11-16 13:47:12 +0530

    changed filter method signature and added first command

---
+++ Shunya.Selenium/Query/FirstCommand.cs
@@ -0,0 +1,23 @@
+// Author:- raj
+// Github:- @OutOfBoundCats
+// Created At:- 16/11/2023/1:41 pm
+
+using System.Collections.ObjectModel;
+using Shunya.Selenium.ExecutionEngine;
+
+namespace Shunya.Selenium.Query;
+
+public static class FirstCommand
+{
+    /// <summary>
+    /// Returns firsdt element from read only collection
+    /// </summary>
+    /// <param name="chain"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IChainable<T> Filter<T>(this IChainable<ReadOnlyCollection<T>> chain)
+    {
+        ActionTaskResult<T> actionResult = new ActionTaskResult<T>(ref chain.GetContext(),chain.GetResult().ElementAt(0));
+        return actionResult;
+    }
+}
